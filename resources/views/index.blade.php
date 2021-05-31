c12466e05bfffc69ecfcd9cd8ff48d0a143acd07
baderdinebenibrahim1 <84507603+baderdinebenibrahim1@users.noreply.github.com>
2021-05-31 14:30:50 +0100

    haka tawa !

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Roulette wheel</title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <link rel="icon" href="whel.png" type="image/gif" sizes="16x16">
+   
+  </head>
+
+  <body class="back" >
+    <h1 id="result" class="result" ></h1>
+    <div id="app">
+      <img class="marker" src="marker.png" />  
+      <img style="margin-top:5px" class="wheel" src="whel.png" />
+      <img class="button" src="button.png" />
+    </div>
+    <div><span class="countdown" id="time">03:30</span></div>
+
+    <script src="script.js"></script>
+  </body>
+</html>
